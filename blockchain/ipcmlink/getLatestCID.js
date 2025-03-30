09bd0b56f0f156bc95cdf6c286a414c12a7fcc23
Shivansh Shrivas <shrivasshivansh@gmail.com>
2025-03-29 21:14:03 -0500

    IPCM smart contract deployed

--- blockchain/ipcmlink/getLatestCID.js
+++
@@ -1,8 +0,0 @@
-async function getLatestCID(meshId) {
-    // TODO: Fetch the latest CID from IPCM contract
-    console.log(`🔍 [IPCM] Fetching latest CID for mesh ${meshId}`);
-    return null; // stub
-  }
-  
-  module.exports = getLatestCID;
-  
--- blockchain/ipcmlink/updateCID.js
+++
@@ -1,7 +0,0 @@
-async function updateCID(meshId, newCID) {
-    // TODO: Implement IPCM smart contract interaction
-    console.log(`🔗 [IPCM] Updated mesh ${meshId} to CID: ${newCID}`);
-  }
-  
-  module.exports = updateCID;
-  
