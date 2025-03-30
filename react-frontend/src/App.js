b3e89952102f6a89eceafd647459925d2134f7fe
Jay Patel <jayrpatel2004>
2025-03-29 23:36:55 -0600

    config device/nickname, mesh selection to main page

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import TextWindow from './components/TextWindow.js';
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DeviceSetup from "./components/DeviceSetup";
+import MeshSelection from "./components/MeshSelection";
+import JoinMesh from "./components/JoinMesh";
+import MainPage from "./pages/MainPage";
+import { AppProvider } from "./context/AppContext";
 
 function App() {
   return (
-    <div style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <TextWindow />
-    </div>
+    <AppProvider>
+      <Router>
+        <div className="w-screen h-screen flex justify-center items-center bg-[#F5F5F0]">
+          <Routes>
+            <Route path="/" element={<DeviceSetup />} />
+            <Route path="/mesh-selection" element={<MeshSelection />} />
+            <Route path="/join-mesh" element={<JoinMesh />} />
+            <Route path="/main" element={<MainPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </AppProvider>
   );
 }
 
